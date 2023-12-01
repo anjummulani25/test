f227263e9278a318951f9c559d7987e77d3d413e
anjum <taamulani25@gmail.com>
2023-12-01 16:35:47 +0530

    first commit

---
+++ myapp/newproject/index.js
@@ -0,0 +1,20 @@
+//<a href="" target="blank">click me to visit</a>
+function customcontainer( reactElement,container)
+{
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    domElement.setAttribute("href", reactElement.props.href);
+    domElement.setAttribute("target", reactElement.props.target);
+    container.appendChild(domElement);
+}
+const reactElement ={
+    type:'a',
+    props:{
+        href:"https://google.com",
+        target:"_blank"
+
+    },
+    children:"click me to visit"
+}
+const maincontainer=document.getElementById("root");
+customcontainer(reactElement,maincontainer);
